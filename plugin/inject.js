be679857162bc2b1b34d6484d79bcfd265fd9527
kishore <kishore@yellowmessenger.com>
2017-06-29 16:16:23 +0530

    Chrome plugin

--- plugin/inject.js
+++ plugin/inject.js
@@ -5,9 +5,6 @@
         success: function(data){
             if(data.success){
                 var body = $('body');
-                if(data.data.botId==="bot_1494260176641"){
-                    body.append($('<script />').attr('src','https://chat.botplatform.io/js/finserv-expert.js').attr("type","text/javascript"));
-                }
                 body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&init=true").attr("type","text/javascript"));
             }
         }
