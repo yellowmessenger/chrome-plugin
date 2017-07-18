5122662ca2026668271fa3d4fc0f0d57290e47c0
kishore <kishore@yellowmessenger.com>
2017-07-18 09:42:33 +0530

    Chrome plugin

--- plugin/inject.js
+++ plugin/inject.js
@@ -5,7 +5,7 @@
         success: function(data){
             if(data.success){
                 var body = $('body');
-                body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&init=true").attr("type","text/javascript"));
+                body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&init=true"+(data.data.botId==="bot_1494260176641"?"&view=compact":"")).attr("type","text/javascript"));
             }
         }
     });
