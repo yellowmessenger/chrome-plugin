da208d35a763193007ae87287b29f1866a2ab1ec
kishore <kishore@yellowmessenger.com>
2017-08-17 14:56:00 +0530

    Chrome plugin

--- plugin/inject.js
+++ plugin/inject.js
@@ -5,24 +5,27 @@
         success: function(data){
             if(data.success){
                 var body = $('body');
+
                 // Don't show at login
-                if((window.location.toString().indexOf("/B2C_CustomLogin.aspx") !== -1 || window.location.toString().toLowerCase().indexOf("logout") !== -1 ) && data.data.botId==="bot_1494260176641"){
-                    return;
-                }
-
-                if(document.referrer.toString().indexOf("/B2C_CustomLogin.aspx") !== -1 && data.data.botId==="bot_1494260176641"){
-                    body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&init=true&open=false&clear=true"+(data.data.botId==="bot_1494260176641"?"&view=compact":"")).attr("type","text/javascript"));
-                    return;
-                }
-
                 if(data.data.botId==="bot_1494260176641"){
+                    if(window.location.toString().indexOf("/B2C_CustomLogin.aspx") !== -1 || window.location.toString().toLowerCase().indexOf("logout") !== -1 ){
+                        return;
+                    }
+                    var loginId = parseInt($($('#welcomeMenuBox a')[0]).text());
+                    var loginName = $('.profile_name').text();
+
+                    if(document.referrer.toString().indexOf("/B2C_CustomLogin.aspx") !== -1){
+                        body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&userId="+loginId+"&name="+loginName+"&init=true&open=false&clear=true"+(data.data.botId==="bot_1494260176641"?"&view=compact":"")).attr("type","text/javascript"));
+                        return;
+                    }
+
                     var open = false;
                     var search = location.search.substring(1);
                     if(search){
                         var params = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
                         open = !!(params && params.bot_event);
                     }
-                    body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&init=true&open="+open+""+(data.data.botId==="bot_1494260176641"?"&view=compact":"")).attr("type","text/javascript"));
+                    body.append($('<script />').attr('src','https://chat.botplatform.io/plugin-v2.js?bot='+data.data.botId+"&userId="+loginId+"&name="+loginName+"&init=true&open="+open+""+(data.data.botId==="bot_1494260176641"?"&view=compact":"")).attr("type","text/javascript"));
                     return;
                 }
 
